93c66a312c82ec7f6c717f490bd64e80f08142f3
Hallef <hallefbruno@hotmail.com>
2021-05-28 07:27:59 -0300

    Modificação

--- src/main/resources/static/js/Index.js
+++ src/main/resources/static/js/Index.js
@@ -1,21 +1,22 @@
 $(function () {
+    $.ajax({
+        url: "numeroPonto",
+        method: "get",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+
+            var dados = data;
+
+            $("#umidadeDoAr").text(dados.manha+"-"+dados.tarde+"-"+dados.noite);
+            $("#iuv").text(dados.iuvMaximo);
+            $("#temperatura").text(dados.temperaturaMinima+"-"+dados.temperaturaMaxima);
+            $("#sol").text(dados.nascerDoSol+"-"+dados.porDoSol);
+
+            window.console.log(data);
+        }
+    });
     $("#btnVerificarPonto").click(function () {
-        $.ajax({
-            url: "numeroPonto",
-            method: "get",
-            contentType: "application/json",
-            dataType: "json",
-            success: function (data) {
-                
-                var dados = data;
-                
-                $("#umidadeDoAr").val(dados.manha+"-"+dados.tarde+"-"+dados.noite);
-                $("#iuv").val(dados.iuvMaximo);
-                $("#temperatura").val(dados.temperaturaMinima+"-"+dados.temperaturaMaxima);
-                $("#sol").val(dados.nascerDoSol+"-"+dados.porDoSol);
-                
-                window.console.log(data);
-            }
-        });
+        alert("Ainda em desenvolvimento!");
     });
 });
