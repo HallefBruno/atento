087120e844408c92951a30f634394ef8c1d0f2c7
Hallef <hallefbruno@hotmail.com>
2021-06-19 15:31:22 -0300

    Projeto parcialmente finalizado

--- src/main/resources/static/js/Index.js
+++ src/main/resources/static/js/Index.js
@@ -4,21 +4,40 @@ $(function () {
         method: "get",
         contentType: "application/json",
         dataType: "json",
+        beforeSend: function () {
+            $("#divLoading").addClass("show");
+        },
         success: function (data) {
-
             var dados = data;
-
-            $("#umidadeDoAr").text(dados.manha+"-"+dados.tarde+"-"+dados.noite);
+            $("#umidadeDoArManha").text(dados.manha);
+            $("#umidadeDoArTarde").text(dados.tarde);
+            $("#umidadeDoArNoite").text(dados.noite);
             $("#iuv").text(dados.iuvMaximo);
-            $("#temperatura").text(dados.temperaturaMinima+"-"+dados.temperaturaMaxima);
-            $("#sol").text(dados.nascerDoSol+"-"+dados.porDoSol);
-
+            $("#temperaturaMinima").text(dados.temperaturaMinima);
+            $("#temperaturaMaxima").text(dados.temperaturaMaxima);
+            $("#nascerDoSol").text(dados.nascerDoSol);
+            $("#porDoSol").text(dados.porDoSol);
+            
+            if(Number(dados.temperaturaMinima.replace(/[^0-9]/g,'').replace("°","")) <= 12) {
+                $("#DiaComSol").css("display","block");
+                $("#DiaQuente").css("display","none");
+                $("#DiaMaisFrio").css("display","none");
+            } else if(Number(dados.temperaturaMinima.replace(/[^0-9]/g,'').replace("°","")) > 12) {
+                $("#DiaQuente").css("display","block");
+                $("#DiaComSol").css("display","none");
+                $("#DiaMaisFrio").css("display","none");
+            } else {
+                $("#DiaMaisFrio").css("display","block");
+                $("#DiaComSol").css("display","none");
+                $("#DiaQuente").css("display","none");
+            }
+            
             window.console.log(data);
         }
     });
-    
-    $(document).ajaxStart(function() {$("#carregando").addClass("show");});
-    $(document).ajaxComplete(function() { $("#carregando").removeClass("show"); });
+    $(document).ajaxComplete(function (event, jqxhr, settings) {
+        $("#divLoading").removeClass("show");
+    });
     
     $("#btnVerificarPonto").click(function () {
         alert("Ainda em desenvolvimento!");
